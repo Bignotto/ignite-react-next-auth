a3bcd9c9b143586a35a81eb4863b9a509ddc5cda
Thiago Bignotto <bignotto@gmail.com>
2021-06-30 06:36:16 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { FormEvent, useState } from "react";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  function handleSubmint(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({ email, pass });
+  }
+
+  return (
+    <form onSubmit={handleSubmint}>
+      <label htmlFor="email">E-Mail</label>
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="text"
+        name="password"
+        value={pass}
+        onChange={e => setPass(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
