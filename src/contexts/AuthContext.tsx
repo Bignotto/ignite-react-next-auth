c522f282fe36cbc3b0f1405fc914e98a15f17934
Thiago Bignotto <bignotto@gmail.com>
2021-07-01 06:47:34 -0300

    added nookies and saved data into a cookie

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from "nookies";
 import Router from "next/router";
 import { api } from "../services/api";
 
@@ -29,6 +30,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", {
@@ -36,12 +47,24 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
 
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: "/",
+      });
+
       setUser({
         email,
-        permissions: response.data?.permissions,
-        roles: response.data?.roles,
+        permissions,
+        roles,
       });
-      console.log(response.data);
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
       Router.push("/dashboard");
     } catch (error) {
       console.log(error);
